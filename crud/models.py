e868c223da35746460dd70ce832a529cbde3a6c8
Mauricio Galeas <mauro.gm.92@gmail.com>
2020-04-19 20:36:54 -0400

    [develop] database and settings set

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+class Person(models.Model):
+ rut = models.CharField(max_length=20)
+ name = models.CharField(max_length=20)
+ lastName = models.CharField(max_length=20)
+ age=models.IntegerField(blank=False)
+ course=models.CharField(max_length=50)
 
-# Create your models here.
+ 
+ def __str__(self):
+  return self.rut,self.name,self.lastName,self.age,self.course
